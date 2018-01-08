d3b0fddd8ed915229c58a96cc18bfc7479eddc91
zicla <ziclax@163.com>
2018-01-09 02:17:03 +0800

    Change the pack shell.

--- doc/script/pack.sh
+++ doc/script/pack.sh
@@ -7,10 +7,14 @@ FINAL_NAME=$VERSION_NAME.linux.tar.gz
 
 DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 
+cd $DIR
+cd ../../
+echo "maven install"
+./mvnw install
+cd ./target
+
 echo "compress to tar.gz"
 echo "tar -zcvf ./$FINAL_NAME $VERSION_NAME.jar $DIR/startup.sh $DIR/shutdown.sh"
-cd $DIR
-cd ../../target
 tar -zcvf ./$FINAL_NAME $VERSION_NAME.jar $DIR/startup.sh $DIR/shutdown.sh
 
 echo "check the dist file in $(pwd)"
