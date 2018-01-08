ccb3db6958b7f7b5918708312ab464f79e33eb1f
zicla <ziclax@163.com>
2018-01-09 01:35:31 +0800

    Update the scripts.

--- doc/script/shutdown.sh
+++ doc/script/shutdown.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "$0"  )" && pwd  )"
-JAR_DIR=$(dirname $DIR)
-JAR_PATH=$JAR_DIR/blog-1.0.0.jar
+JAR_PATH=$DIR/blog-1.0.0.jar
 
 EDASPID=`ps -ef | grep "java .* -jar $JAR_PATH"|grep -v grep |head -n 1 | awk '{print $2}'`
 if [ -z $EDASPID ];
-then 
+then
         echo "Cannot find $JAR_PATH."
 else
         kill -9 $EDASPID
         echo $JAR_PATH
         echo 'Shutdown successfully.'
 fi
-
-#rm jar folder
-rm -rf ${JAR_PATH%.jar}
--- doc/script/startup.sh
+++ doc/script/startup.sh
@@ -2,20 +2,19 @@
 
 # executable path
 DIR="$( cd "$( dirname "$0"  )" && pwd  )"
-JAR_DIR=$(dirname $DIR)
-JAR_PATH=$JAR_DIR/blog-1.0.0.jar
+JAR_PATH=$DIR/blog-1.0.0.jar
 
 #configs
 ServerPort=6020
 #mysql configs
-MysqlPort=3306,
+MysqlPort=3306
 MysqlHost=127.0.0.1
-MysqlSchema=tank
-MysqlUserName=tank
-MysqlPassword=tank123
-MysqlUrl=jdbc:mysql://$MysqlHost:$MysqlPort/$MysqlSchema?useUnicode=true&characterEncoding=UTF-8&useSSL=false
+MysqlSchema=blog
+MysqlUserName=blog
+MysqlPassword=blog123
+MysqlUrl="jdbc:mysql://$MysqlHost:$MysqlPort/$MysqlSchema?useUnicode=true&characterEncoding=UTF-8&useSSL=false"
 #tank configs
-TankUrl=http://tank.eyeblue.cn
+TankUrl="http://tank.eyeblue.cn"
 TankEmail=blog_dev@tank.eyeblue.cn
 TankPassword=123456
 #admin configs
@@ -30,17 +29,21 @@ MailUsername=ziclax@126.com
 MailPassword=Ziclax123
 MailDefaultEncoding=UTF-8
 
-OPTS="-Xmx512m -Dserver.port=$ServerPort -Dspring.datasource.url=\"$MysqlUrl\" -Dspring.datasource.username=$MysqlUserName -Dspring.datasource.password=$MysqlPassword -Dtank.url=\"$TankUrl\" -Dtank.email=$TankEmail -Dtank.password=$TankPassword -Dadmin.username=$AdminUsername -Dadmin.email=$AdminEmail -Dadmin.password=$AdminPassword -Dspring.mail.protocol=$MailProtocol -Dspring.mail.host=$MailHost -Dspring.mail.port=$MailPort -Dspring.mail.username=$MailUsername -Dspring.mail.password=$MailPassword -Dspring.mail.default-encoding=$MailDefaultEncoding"
+OPTS="-Xmx512m -Dserver.port=$ServerPort -Dspring.datasource.url=$MysqlUrl -Dspring.datasource.username=$MysqlUserName -Dspring.datasource.password=$MysqlPassword -Dtank.url=$TankUrl -Dtank.email=$TankEmail -Dtank.password=$TankPassword -Dadmin.username=$AdminUsername -Dadmin.email=$AdminEmail -Dadmin.password=$AdminPassword -Dspring.mail.protocol=$MailProtocol -Dspring.mail.host=$MailHost -Dspring.mail.port=$MailPort -Dspring.mail.username=$MailUsername -Dspring.mail.password=$MailPassword -Dspring.mail.default-encoding=$MailDefaultEncoding"
+
+
+echo $OPTS
+
 
 JAVA='java'
 if [ -z `which java` ]; then
-    if [ -z $JAVA_HOME ]; 
+    if [ -z $JAVA_HOME ];
         then
             JAVA=$JAVA_HOME/bin/java
         else
             echo 'Cannot find java command and JAVA_HOME.'
     fi
-fi 
+fi
 
 if [ ! -z `java -version 2>&1 | grep 'java version' | awk '{print $3}' | egrep '1.[8-9].\d*'` ]; then
     nohup $JAVA $OPTS -jar $JAR_PATH >/dev/null 2>&1 &
