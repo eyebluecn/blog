d01d25d9496c5f67fe2ea6daa5456ab5a2c8541e
zicla <ziclax@126.com>
2018-03-01 18:16:15 +0800

    fix the log file bug

--- src/main/java/cn/eyeblue/blog/config/bootstrap/ApplicationBootstrap.java
+++ src/main/java/cn/eyeblue/blog/config/bootstrap/ApplicationBootstrap.java
@@ -25,13 +25,6 @@ public class ApplicationBootstrap implements ApplicationListener<ContextRefreshe
 
         log.info("---------------------Bootstrap Blog " + config.getBlogVersion() + "------------------------------");
 
-
-        log.debug("这里是debug信息");
-        log.info("这里是info信息");
-        log.warn("这里是warn信息");
-        log.error("这里是err信息");
-
-
         try {
             installService.start();
         } catch (Exception e) {
